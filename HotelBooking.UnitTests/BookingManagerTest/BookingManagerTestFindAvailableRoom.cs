bf994592a52e5527192678b3d910aebe67615fbb
Jens <55052002+TheDarkSide1992@users.noreply.github.com>
2025-09-16 12:32:04 +0200

    Feature/find available roms/moq (#5)
    * Created Moq Insted of fake repostitory
    
    * Formating
    
    * Corected spelling mistake and magic numers

--- HotelBooking.UnitTests/BookingManagerTest/BookingManagerTestFindAvailableRoom.cs
+++ HotelBooking.UnitTests/BookingManagerTest/BookingManagerTestFindAvailableRoom.cs
@@ -2,74 +2,96 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using HotelBooking.Core;
-using HotelBooking.UnitTests.Fakes;
+using Moq;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace HotelBooking.UnitTests.BookingManagerTest;
 
 public class BookingManagerTestFindAvailableRoom
 {
     private IBookingManager bookingManager;
-    private IRepository<Booking> bookingRepository;
+    private readonly int noRoomsReturned = -1;
 
-    public BookingManagerTestFindAvailableRoom(){
+    public BookingManagerTestFindAvailableRoom()
+    {
         DateTime start = DateTime.Today.AddDays(10);
         DateTime end = DateTime.Today.AddDays(20);
-        bookingRepository = new FakeBookingRepository(start, end);
-        IRepository<Room> roomRepository = new FakeRoomRepository();
-        bookingManager = new BookingManager(bookingRepository, roomRepository);
+        Mock<IRepository<Booking>> bookingRepository = new Mock<IRepository<Booking>>();
+        Mock<IRepository<Room>> roomRepository = new Mock<IRepository<Room>>();
+
+        var bookings = new[] // Mocks booking repository
+        {
+            new Booking
+            {
+                Id = 1, StartDate = start, EndDate = end, IsActive = true,
+                CustomerId = 1, RoomId = 1
+            },
+            new Booking
+            {
+                Id = 2, StartDate = start, EndDate = end, IsActive = true,
+                CustomerId = 2, RoomId = 2
+            },
+        };
+        bookingRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(bookings);
+
+        var rooms = new[] //mock data for Rooms repository
+        {
+            new Room { Id = 1, Description = "A" },
+            new Room { Id = 2, Description = "B" }
+        };
+        roomRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(rooms);
+
+        bookingManager = new BookingManager(bookingRepository.Object, roomRepository.Object);
     }
-    
-    public static IEnumerable<object[]> GetPreDates() 
+
+    public static IEnumerable<object[]> GetPreDates()
     {
         yield return new object[] { null };
         yield return new object[] { DateTime.Today };
-        yield return new object[] { new DateTime(2025,1,1) };
-        yield return new object[] { new DateTime(2025,4,8) };
-        yield return new object[] { new DateTime(2025,4,2) };
-        yield return new object[] { new DateTime(2025,6,5) };
-        yield return new object[] { new DateTime(2005,5,5) };
-        yield return new object[] { new DateTime(2001,1,1) };
-        yield return new object[] { new DateTime(1999,12,31) };
-        yield return new object[] { new DateTime(1986,8,28) };
+        yield return new object[] { DateTime.Today.AddDays(-100) };
+        yield return new object[] { DateTime.Today.AddDays(-50) };
+        yield return new object[] { DateTime.Today.AddDays(-56) };
+        yield return new object[] { DateTime.Today.AddDays(-20) };
+        yield return new object[] { DateTime.Today.AddDays(-7400) };
+        yield return new object[] { DateTime.Today.AddDays(-9000) };
+        yield return new object[] { DateTime.Today.AddDays(-9300) };
+        yield return new object[] { DateTime.Today.AddDays(-14200) };
     }
-    
-    public static IEnumerable<object[]> GetFutureDates() 
+
+    public static IEnumerable<object[]> GetFutureDates()
     {
-        yield return new object[] { DateTime.Today.AddDays(21), DateTime.Today.AddDays(30)  };
-        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(2)  };
-        yield return new object[] { DateTime.Today.AddDays(37), DateTime.Today.AddDays(44)  };
-        yield return new object[] { DateTime.Today.AddDays(220), DateTime.Today.AddDays(225)  };
-        yield return new object[] { DateTime.Today.AddDays(336), DateTime.Today.AddDays(338)  };
-        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(5)  };
-        yield return new object[] { DateTime.Today.AddDays(7), DateTime.Today.AddDays(9)  };
-        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(6)  };
-        yield return new object[] { DateTime.Today.AddDays(3), DateTime.Today.AddDays(7)  };
+        yield return new object[] { DateTime.Today.AddDays(21), DateTime.Today.AddDays(30) };
+        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(2) };
+        yield return new object[] { DateTime.Today.AddDays(37), DateTime.Today.AddDays(44) };
+        yield return new object[] { DateTime.Today.AddDays(220), DateTime.Today.AddDays(225) };
+        yield return new object[] { DateTime.Today.AddDays(336), DateTime.Today.AddDays(338) };
+        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(5) };
+        yield return new object[] { DateTime.Today.AddDays(7), DateTime.Today.AddDays(9) };
+        yield return new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(6) };
+        yield return new object[] { DateTime.Today.AddDays(3), DateTime.Today.AddDays(7) };
     }
 
     public static IEnumerable<object[]> GetReservedDates()
     {
-        yield return new object[] { DateTime.Today.AddDays(11), DateTime.Today.AddDays(15)  };
-        yield return new object[] { DateTime.Today.AddDays(10), DateTime.Today.AddDays(20)  };
-        yield return new object[] { DateTime.Today.AddDays(18), DateTime.Today.AddDays(22)  };
-        yield return new object[] { DateTime.Today.AddDays(8), DateTime.Today.AddDays(14)  };
-        yield return new object[] { DateTime.Today.AddDays(11), DateTime.Today.AddDays(31)  };
-
+        yield return new object[] { DateTime.Today.AddDays(11), DateTime.Today.AddDays(15) };
+        yield return new object[] { DateTime.Today.AddDays(10), DateTime.Today.AddDays(20) };
+        yield return new object[] { DateTime.Today.AddDays(18), DateTime.Today.AddDays(22) };
+        yield return new object[] { DateTime.Today.AddDays(8), DateTime.Today.AddDays(14) };
+        yield return new object[] { DateTime.Today.AddDays(11), DateTime.Today.AddDays(31) };
     }
 
     public static IEnumerable<object[]> GetUnrealisticDates()
     {
-        yield return new object[] { DateTime.Today.AddDays(32), DateTime.Today.AddDays(21)  };
-        yield return new object[] { DateTime.Today.AddDays(8), DateTime.Today.AddDays(2)  };
-        yield return new object[] { DateTime.Today.AddDays(68), DateTime.Today.AddDays(52)  };
-        yield return new object[] { DateTime.Today.AddDays(70), DateTime.Today.AddDays(69)  };
-        yield return new object[] { DateTime.Today.AddDays(336), DateTime.Today.AddDays(228)  };
-        yield return new object[] { DateTime.Today.AddDays(332), DateTime.Today.AddDays(310)  };
+        yield return new object[] { DateTime.Today.AddDays(32), DateTime.Today.AddDays(21) };
+        yield return new object[] { DateTime.Today.AddDays(8), DateTime.Today.AddDays(2) };
+        yield return new object[] { DateTime.Today.AddDays(68), DateTime.Today.AddDays(52) };
+        yield return new object[] { DateTime.Today.AddDays(70), DateTime.Today.AddDays(69) };
+        yield return new object[] { DateTime.Today.AddDays(336), DateTime.Today.AddDays(228) };
+        yield return new object[] { DateTime.Today.AddDays(332), DateTime.Today.AddDays(310) };
     }
 
     [Theory]
-    [MemberData(nameof(GetPreDates))] //Arange
+    [MemberData(nameof(GetPreDates))]
     public async Task FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException(DateTime date)
     {
         // Act
@@ -87,23 +109,26 @@ public class BookingManagerTestFindAvailableRoom
         int result = await bookingManager.FindAvailableRoom(startDate, endDate);
 
         // Assert
-        Assert.NotEqual(-1 , result);
+        Assert.NotEqual(noRoomsReturned, result);
         Assert.IsType<int>(result);
     }
 
     [Theory]
     [MemberData(nameof(GetReservedDates))]
-    public async Task FindAvailebelRoms_FutureDateReserved_NoRoomsAvailable_MinusOne(DateTime startDate, DateTime endDate)
+    public async Task FindAvailableRooms_FutureDateReserved_NoRoomsAvailable_MinusOne(DateTime startDate,
+        DateTime endDate)
     {
-        int result = await bookingManager.FindAvailableRoom(startDate,endDate);
-        
-        Assert.Equal(-1, result); //Assert result is equal to minus one for no availebel rooms
+        //Act
+        int result = await bookingManager.FindAvailableRoom(startDate, endDate);
 
+        //Assert
+        Assert.Equal(noRoomsReturned, result);
     }
-    
+
     [Theory]
     [MemberData(nameof(GetUnrealisticDates))]
-    public async Task FindAvailebelRoms_StartDateHigherThanEndDate_ArgumentException(DateTime startDate, DateTime endDate)
+    public async Task FindAvailableRooms_StartDateHigherThanEndDate_ArgumentException(DateTime startDate,
+        DateTime endDate)
     {
         // Act
         Task result() => bookingManager.FindAvailableRoom(startDate, endDate);
@@ -111,5 +136,4 @@ public class BookingManagerTestFindAvailableRoom
         // Assert
         await Assert.ThrowsAsync<ArgumentException>(result);
     }
-
 }
